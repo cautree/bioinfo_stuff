dbc6eb1f52e40798c0ed1ad16420145c916b5e94
Yanyan Liu <yanyanliujs@gmail.com>
2023-09-25 21:21:52 -0400

    update get demux

--- bash_stuff/get_demux_report.sh
+++ bash_stuff/get_demux_report.sh
@@ -9,11 +9,16 @@ rm -r file_to_get
 fi
 
 
-aws s3 ls s3://seqwell-fastq/$1/ | grep -e txt | awk '{$1=$1}1' OFS="," | cut -f 4 -d "," > file_to_get
-aws s3 ls s3://seqwell-fastq/$1/ | grep -e xlsx | awk '{$1=$1}1' OFS="," | cut -f 4 -d "," >> file_to_get
-aws s3 ls s3://seqwell-fastq/$1/ | grep -e csv | awk '{$1=$1}1' OFS="," | cut -f 4 -d "," >> file_to_get
+aws s3 ls s3://seqwell-fastq/$1/ \
+| grep  'txt\|xlsx\|csv' \
+| awk '{$1=$1}1' OFS="," \
+| cut -f 4 -d "," > file_to_get
+
 
 
 while read line; do
 aws s3 cp s3://seqwell-fastq/$1/$line demux_report
 done < file_to_get
+
+
+touch demux_report/*
