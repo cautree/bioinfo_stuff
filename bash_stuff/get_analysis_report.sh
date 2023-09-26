f6406cd2f21f7f19b72ab6d8f4df1a3f1de4f1e9
Yanyan Liu <yanyanliujs@gmail.com>
2023-09-25 21:13:01 -0400

    update get analysis report

--- bash_stuff/get_analysis_report.sh
+++ bash_stuff/get_analysis_report.sh
@@ -10,9 +10,10 @@ fi
 
 mkdir analysis_report
 
-aws s3 ls s3://seqwell-analysis/$1/$2/ | grep -e csv | awk '{$1=$1}1' OFS="," | cut -f 4 -d "," > file_to_get
-aws s3 ls s3://seqwell-analysis/$1/$2/ | grep -e xlsx | awk '{$1=$1}1' OFS="," | cut -f 4 -d "," >> file_to_get
-
+aws s3 ls s3://seqwell-analysis/$1/$2/ \
+| grep 'csv\|xlsx' \
+| awk '{$1=$1}1' OFS="," | \
+cut -f 4 -d "," > file_to_get
 
 while read line; do
 aws s3 cp s3://seqwell-analysis/$1/$2/$line analysis_report
