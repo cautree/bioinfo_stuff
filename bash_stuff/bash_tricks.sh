7dff47754e6dfc36bb70ca205ca00589a9eb3d59
Yanyan Liu <yanyanliujs@gmail.com>
2023-09-26 22:07:31 -0400

    add bash tricks

--- bash_stuff/bash_tricks.sh
+++ bash_stuff/bash_tricks.sh
@@ -1,2 +1,8 @@
 ## on mac has to do this way, otherwise zcat: can't stat: 
 zcat < foo.txt.gz 
+
+
+## turn multi line fasta to oneline
+seqtk seq multi-line.fasta > single-line.fasta
+## use this to check 
+seqtk seq celegans_chr1.fa | head -c 170
