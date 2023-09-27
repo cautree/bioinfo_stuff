6c03a1ff69d67c3c9acda0124cc64e50fccde32e
Yanyan Liu <yanyanliujs@gmail.com>
2023-09-27 08:22:41 -0400

    update get ref for awk space remove

--- bash_stuff/get_ref_files_from_s3/get_ref.sh
+++ bash_stuff/get_ref_files_from_s3/get_ref.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-
+#awk '{$1=$1}1' is the trick to remove extra space
+#https://unix.stackexchange.com/questions/102008/how-do-i-trim-leading-and-trailing-whitespace-from-each-line-of-some-output
 aws s3 ls s3://seqwell-ref/ | grep ecoli_REL606 | grep -v _ecoli_ | \
-awk '{$1=$1}1' OFS="," | cut -f 4 -d "," >ecoli_ref_file
+awk '{$1=$1}1' OFS="," | cut -f 4 -d "," > ecoli_ref_file
 
 while read line;
 do
