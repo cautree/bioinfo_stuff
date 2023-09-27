5fe3214c9db0744385ebf6a9bc4b2c05e6b347ea
Yanyan Liu <yanyanliujs@gmail.com>
2023-09-26 22:25:40 -0400

    more bash tricks

--- bash_stuff/bash_tricks.sh
+++ bash_stuff/bash_tricks.sh
@@ -6,3 +6,15 @@ zcat < foo.txt.gz
 seqtk seq multi-line.fasta > single-line.fasta
 ## use this to check 
 seqtk seq celegans_chr1.fa | head -c 170
+
+
+## Split a multi-FASTA file into individual FASTA files by awk
+awk '/^>/{s=++d".fa"} {print > s}' meta_data/ref.fa
+
+## Split a multi-FASTA file [2 line format] into individual FASTA files by awk
+cat meta_data/ref.fa | split -l 2 - ref/seq_
+
+
+## number of reads in fastq
+cat file.fq | echo $((`wc -l`/4))
+zcat file.fastq.gz | paste - - - - | wc -l 
