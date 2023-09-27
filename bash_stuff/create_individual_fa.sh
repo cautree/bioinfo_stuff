16fae8bfb1c704c5fcbab43ce064091ce06b3bcf
Yanyan Liu <yanyanliujs@gmail.com>
2023-09-26 21:57:07 -0400

    update create individual fa

--- bash_stuff/create_individual_fa.sh
+++ bash_stuff/create_individual_fa.sh
@@ -3,7 +3,7 @@
 mkdir -p ref
 
 #tail -n +2, start from the second row
-cat meta_data/plasmid_sequences_ref.tsv | tail -n +2 | awk 'BEGIN{OFS="\n"} {print ">"$1, $4}' > meta_data/ref.fa
+tail -n +2 meta_data/plasmid_sequences_ref.tsv  | awk 'BEGIN{OFS="\n"} {print ">"$1, $4}' > meta_data/ref.fa
 ## for fasta sequence with 2 lines, can do this, https://www.biostars.org/p/105388/#105402
 cat meta_data/ref.fa | split -l 2 - ref/seq_
 
@@ -12,7 +12,7 @@ ls ref | grep seq_ > file
 ## rename each individual file
 ## cut -c2-, remove ">"
 while read line; do
-filename=$(cat ref/$line | head -1 |cut -c2- ) 
+filename=$(head -1 ref/$line | cut -c2- ) 
 echo $filename
 cat ref/$line > ref/${filename}.fa
 done < file
