78f5e566a5e3b92d373d2647e9809702ca966ff3
Yanyan Liu <yanyanliujs@gmail.com>
2023-10-12 11:48:43 -0400

    add python stuff

---
+++ python_stuff/rename_sample/rename.py
@@ -0,0 +1,19 @@
+#!/opt/anaconda3/bin/python
+
+import pandas as pd
+import os
+
+##put the name.txt and rename.py in the fastq file folder
+
+path = 'name.txt'
+target_names = pd.read_csv(path, sep= "\t")
+name_dict = dict(zip( target_names.old_id, target_names.new_id))
+
+paths = [ path for path in sorted(os.listdir('.'))]
+
+for path in paths:
+    old_id = path
+    out = path.replace(old_id, name_dict[old_id])
+    print(path, out)
+    os.rename(path, out)
+
