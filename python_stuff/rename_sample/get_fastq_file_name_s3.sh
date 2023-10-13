7af7dad99a2095fa53be11488d09ae75c058b6b4
Yanyan Liu <yanyanliujs@gmail.com>
2023-10-13 08:19:25 -0400

    update get_fastq_file_name_s3.sh

--- python_stuff/rename_sample/get_fastq_file_name_s3.sh
+++ python_stuff/rename_sample/get_fastq_file_name_s3.sh
@@ -7,6 +7,7 @@ plate_name=$3
 
 
 #aws s3 ls s3://seqwell-fastq/20231011_MiSeq-Sharkboy/bota_plasmids_FASTQ/ 
+## -n is to get rid of extra line after echo
 echo -n old_id  > old_name
 aws s3 ls s3://${s3_path}/${run_name}/${plate_name}/ | awk '{$1=$1}1' OFS="," | cut -f 4 -d "," >> old_name
 
