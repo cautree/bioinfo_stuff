a66fa52872735400fbea6ab3b68e4398b2958f25
Yanyan Liu <yanyanliujs@gmail.com>
2023-10-16 22:01:43 -0400

    update R vs python

--- R_vs_Python/python_code.py
+++ R_vs_Python/python_code.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+## method1: small data
 df = pd.DataFrame( { "Yes": [50,21],
                      "No": [131,2], 
                      })
@@ -8,11 +9,25 @@ print(df.head())
 
 
 
-
+## method2: from pd.Series
 s1 = pd.Series([1,2,3,4,5])
 s2 = pd.Series([2,3,4,5,6])
 df = pd.concat([s1,s2],axis=1)
 print(df.head())
+
+## method3: data can be from list, list comprehension
+df_insert_size = pd.DataFrame(range(1,1001), columns = ['insert_size'])
+print(df_insert_size.head())
+
+
+## method4: list of list
+data = []
+samp, tpm_1, tpm_2 = ['a', 1, 10]
+data += [[samp, tpm_1, tpm_2]]
+data += [[samp, tpm_1, tpm_2]]
+df_tpm = pd.DataFrame( data, columns= ['samp', 'tpm_1', 'tpm_2'])
+print(df_tpm)
+
 
 
 df = pd.read_csv("mtcars.csv")
@@ -33,8 +48,13 @@ print(df.head())
 print(df.index)
 
 
-## filter by column value
+## filter by column value using loc
 print(df.loc[df.mpg >25])
+
+## filter, R style
+print(df[df.mpg >25])
+
+
 
 ## select columns
 df1 = df[ ["mpg","wt"]]
@@ -88,6 +108,12 @@ df3 = df
 df3['am'] = df3.am.astype("str")
 df3['model_am'] = df3.index + '_' + df3.am
 print(df3.model_am)
+
+## drop a column
+df4 =df3.copy(deep=True)
+df4.drop( ['model_am', 'am'], axis =1, inplace=True)
+print(df3.shape) #12
+print(df4.shape) #10
 
 
 ## group and counts
@@ -159,6 +185,8 @@ dfc.set_index( 'model', inplace=True)
 dfd.set_index( 'model', inplace=True)
 dfcd = dfc.merge(dfd, left_index = True, right_index= True )
 print(dfcd.shape[1] == dfc.shape[1] + dfd.shape[1]) # true
+dfcd.sort_index( inplace=True )
+print(dfcd.head())
 
 ## concatenate, similar to dplyr::bind_rows()
 dfa = pd.read_csv("mtcars3.csv")
