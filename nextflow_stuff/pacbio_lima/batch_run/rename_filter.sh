3448480b6b85cf4c3e5bea069a0a1b5b57b2ae43
Yanyan Liu <yanyanliujs@gmail.com>
2023-10-31 07:57:13 -0400

    add lima rename filter

---
+++ nextflow_stuff/pacbio_lima/batch_run/rename_filter.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+
+mkdir -p bamfile 
+mkdir -p renamed_bamfile
+cp *.bam bamfile/
+ls -1 bamfile > file
+while read line; do
+name=$(basename bamfile/$line .bam)
+  
+newname=${params.date}_\$(echo \$name | tr -d '-' | cut -d. -f2- | sed 's/seqwell//g' | tr -d '.')
+
+a=$(echo $newname | cut -d_ -f3)
+b=$(echo $newname | cut -d_ -f5)
+
+if [ $a == $b ]
+then
+    mv bamfile/${line} renamed_bamfile/${newname}.bam
+fi
+
+  
+done < file
+mv renamed_bamfile/* .
+rm -rf bamfile
+rm -rf renamed_bamfile
