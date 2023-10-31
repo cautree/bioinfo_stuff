b89572977741e44fd5ee6ab6ebceda014daf68ae
Yanyan Liu <yanyanliujs@gmail.com>
2023-10-31 08:00:14 -0400

    update lima rename filtering

--- nextflow_stuff/pacbio_lima/batch_run/rename.sh
+++ nextflow_stuff/pacbio_lima/batch_run/rename.sh
@@ -1,17 +1,17 @@
 #!bin/bash
 
 
-  mkdir -p bamfile 
-  mkdir -p renamed_bamfile
-  cp *.bam bamfile/
-  ls -1 bamfile > file
-  while read line; do
-  name=$(basename bamfile/$line .bam)
+mkdir -p bamfile 
+mkdir -p renamed_bamfile
+cp *.bam bamfile/
+ls -1 bamfile > file
+while read line; do
+name=$(basename bamfile/$line .bam)
   
-  newname=${params.date}_\$(echo \$name | tr -d '-' | cut -d. -f2- | sed 's/seqwell//g' | tr -d '.')
-  mv bamfile/${line} renamed_bamfile/${newname}.bam
+newname=${params.date}_\$(echo \$name | tr -d '-' | cut -d. -f2- | sed 's/seqwell//g' | tr -d '.')
+mv bamfile/${line} renamed_bamfile/${newname}.bam
   
-  done < file
-  mv renamed_bamfile/* .
-  rm -rf bamfile
-  rm -rf renamed_bamfile
+done < file
+mv renamed_bamfile/* .
+rm -rf bamfile
+rm -rf renamed_bamfile
--- nextflow_stuff/pacbio_lima/batch_run/rename_filter.sh
+++ nextflow_stuff/pacbio_lima/batch_run/rename_filter.sh
@@ -1,6 +1,5 @@
 #!bin/bash
 
-
 mkdir -p bamfile 
 mkdir -p renamed_bamfile
 cp *.bam bamfile/
@@ -10,6 +9,7 @@ name=$(basename bamfile/$line .bam)
   
 newname=${params.date}_\$(echo \$name | tr -d '-' | cut -d. -f2- | sed 's/seqwell//g' | tr -d '.')
 
+##filter out crosstalk ones
 a=$(echo $newname | cut -d_ -f3)
 b=$(echo $newname | cut -d_ -f5)
 
@@ -17,8 +17,7 @@ if [ $a == $b ]
 then
     mv bamfile/${line} renamed_bamfile/${newname}.bam
 fi
-
-  
+ 
 done < file
 mv renamed_bamfile/* .
 rm -rf bamfile
